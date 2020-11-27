5e00ce371f7cbbd11868dbd5727e03c2cf433883
yash handa <yashhanda500@gmail.com>
2020-11-27 23:21:47 +0530

    update

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends React.Component {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Reac
+          Learn React
         </a>
       </header>
     </div>
